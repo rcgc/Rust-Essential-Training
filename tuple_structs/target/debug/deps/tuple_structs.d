726cf67a196791e876aa0b2cef85813e1de679dd
rcgc <roberto140298@gmail.com>
2023-10-15 16:23:56 -0600

    struct tupples tutorial finished

---
+++ tuple_structs/target/debug/deps/tuple_structs.d
@@ -0,0 +1,5 @@
+C:\Users\RobertoCarlos\Documents\Rust-Essential-Training\tuple_structs\target\debug\deps\tuple_structs.exe: src\main.rs
+
+C:\Users\RobertoCarlos\Documents\Rust-Essential-Training\tuple_structs\target\debug\deps\tuple_structs.d: src\main.rs
+
+src\main.rs:
---
+++ tuple_structs/target/debug/tuple_structs.d
@@ -0,0 +1,1 @@
+C:\Users\RobertoCarlos\Documents\Rust-Essential-Training\tuple_structs\target\debug\tuple_structs.exe: C:\Users\RobertoCarlos\Documents\Rust-Essential-Training\tuple_structs\src\main.rs
