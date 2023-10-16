ea2f5913e6d9695c2f793596360ad69ac682358c
rcgc <roberto140298@gmail.com>
2023-10-16 08:15:12 -0600

    challenge9_enums finished

---
+++ challenge9_enums/target/debug/challenge9_enums.d
@@ -0,0 +1,1 @@
+C:\Users\RobertoCarlos\Documents\Rust-Essential-Training\challenge9_enums\target\debug\challenge9_enums.exe: C:\Users\RobertoCarlos\Documents\Rust-Essential-Training\challenge9_enums\src\main.rs
---
+++ challenge9_enums/target/debug/deps/challenge9_enums.d
@@ -0,0 +1,5 @@
+C:\Users\RobertoCarlos\Documents\Rust-Essential-Training\challenge9_enums\target\debug\deps\challenge9_enums.exe: src\main.rs
+
+C:\Users\RobertoCarlos\Documents\Rust-Essential-Training\challenge9_enums\target\debug\deps\challenge9_enums.d: src\main.rs
+
+src\main.rs:
